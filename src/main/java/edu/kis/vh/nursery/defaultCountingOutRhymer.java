e598a49fb0da1545f2e1625844c250977c8ddb30
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-01-26 11:10:02 +0100

    zmiana nazwy metody

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -19,7 +19,7 @@ public class defaultCountingOutRhymer {
         return total == 11;
     }
 
-    protected int peekaboo() {
+    protected int getNumber() {
         if (callCheck())
             return -1;
         return numbers[total];
